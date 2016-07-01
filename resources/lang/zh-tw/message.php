b5f9bde76133592765534a583c7f6bc18547c078
Jack Sun <sunhsilu@gmail.com>
2016-07-01 11:35:23 +0800

    Add Chinese Traditional localization

---
+++ resources/lang/zh-tw/message.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'difference'    => '差異',
+    'translations'  => '翻譯',
+    'instructions'  => '說明',
+    'steps'         => '
+        <br>
+        <strong>步驟一：</strong> 啟用您想翻譯的語言項目（在 設定 &gt; 系統）。<br>
+        <strong>步驟二：</strong> 探索並翻譯你想要的附加元件或系統項目。翻譯檔將會存放在對應的資料夾中。<br>
+        <strong>步驟三：</strong> 翻譯後，請送出 PR 到正確的 Git Repo 中。'
+];
