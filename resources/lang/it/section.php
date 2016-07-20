117042e3357a6a2f804da6d93702cdc20991ba12
Christian Giupponi <chri788@gmail.com>
2016-07-20 10:49:44 +0200

    Add italian translation

---
+++ resources/lang/it/section.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+    'instructions' => [
+        'title' => 'Istruzioni',
+    ],
+    'system' => [
+        'title' => 'Sistema',
+    ],
+    'modules' => [
+        'title' => 'Moduli',
+    ],
+    'themes' => [
+        'title' => 'Temi',
+    ],
+    'plugins' => [
+        'title' => 'Plugin',
+    ],
+    'extensions' => [
+        'title' => 'Estensioni',
+    ],
+    'field_types' => [
+        'title' => 'Field Types',
+    ],
+];
