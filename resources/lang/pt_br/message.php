40416a7b2335ec9c58ea13056042b9085a1604c2
Leonam Pereira Dias <leonam.pd@gmail.com>
2016-08-28 18:46:04 -0300

    Brazilian Portuguese

---
+++ resources/lang/pt_br/message.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'difference'   => 'Diferença',
+    'translations' => 'Tradução',
+    'instructions' => 'Instruções',
+    'steps'        => '
+        <br>
+        <strong>Passo 1:</strong> Habilite os idiomas que você gostaria de traduzir em <a href="/admin/settings">Configurações > Sistema</a>.<br>
+        <strong>Passo 2:</strong> Explore e traduza a(s) extensões ou sistema. As traduções são escritas depois de salvas nos respectivos diretórios de cada uma das extensões<br>
+        <strong>Passo 3:</strong> Submeta um PR com suas traduções!
+        ',
+];
