71e6d86cca67ba909bb409613b7ecc0d554c7f4e
aldf <aladinfajr@gmail.com>
2016-09-09 10:49:29 +0300

    Create section.php

---
+++ resources/lang/ar/section.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+    'instructions' => [
+        'title' => 'التعليمات',
+    ],
+    'system' => [
+        'title' => 'النظام',
+    ],
+    'modules' => [
+        'title' => 'الوحدات',
+    ],
+    'themes' => [
+        'title' => 'السمات',
+    ],
+    'plugins' => [
+        'title' => 'الملحقات',
+    ],
+    'extensions' => [
+        'title' => 'المركبات',
+    ],
+    'field_types' => [
+        'title' => 'أتواع الحقول',
+    ],
+];
