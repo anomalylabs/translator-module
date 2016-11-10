63a47d85fef0d494808a7cc735efd95ceef55415
oimken <oimken@gmail.com>
2016-11-10 19:18:26 +0800

    Simplified Chinese added
    Simplified Chinese added

---
+++ resources/lang/zh-cn/addon.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'title'       => '翻译',
+    'name'        => '翻译模块',
+    'description' => '系统和附加组件的翻译助手.',
+];
---
+++ resources/lang/zh-cn/message.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'difference'   => '差异',
+    'translations' => '翻译',
+    'instructions' => '使用指南',
+    'steps'        => '
+        <br>
+        <strong>Step 1:</strong> 在 <a href="/admin/settings">设置 > 系统</a> 中启用需要翻译的语言.<br>
+        <strong>Step 2:</strong> 查看并翻译附加组件或系统的语言条目. 翻译好的条目会分别保存到各附加组件的对应目录!<br>
+        <strong>Step 3:</strong> 为翻译提交一个 PR 到合适的储存容器(Repository) !
+        ',
+];
