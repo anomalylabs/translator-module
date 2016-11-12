596e7c0117cc4dbde5400ab6d1da964505d57a5e
oimken <oimken@gmail.com>
2016-11-12 23:23:33 +0800

    Simplified Chinese typo

--- resources/lang/zh-cn/message.php
+++ resources/lang/zh-cn/message.php
@@ -6,8 +6,8 @@ return [
     'instructions' => '使用指南',
     'steps'        => '
         <br>
-        <strong>Step 1:</strong> 在 <a href="/admin/settings">设置 > 系统</a> 中启用需要翻译的语言.<br>
-        <strong>Step 2:</strong> 查看并翻译附加组件或系统的语言条目. 翻译好的条目会分别保存到各附加组件的对应目录!<br>
-        <strong>Step 3:</strong> 为翻译提交一个 PR 到合适的储存容器(Repository) !
+        <strong>步骤 1:</strong> 在 <a href="/admin/settings">设置 > 系统</a> 中启用需要翻译的语言.<br>
+        <strong>步骤 2:</strong> 查看并翻译附加组件或系统的语言条目. 翻译好的条目会分别保存到各附加组件的对应目录!<br>
+        <strong>步骤 3:</strong> 完成翻译后提交一个 Pull Request 到对应附加组件的储存仓库 (Repository) !
         ',
 ];
